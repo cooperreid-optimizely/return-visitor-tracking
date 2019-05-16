4986d4c168b36535ad55f19c7188e71b3c63b74e
Cooper Reid <cooper.reid@optimizely.com>
2019-05-16 09:57:37 -0400

    Update ext.js

--- ext.js
+++ ext.js
@@ -1,20 +1,22 @@
-/**
-* if visitor saw the experiment before, in a different session, send "return visitor" event
-* return visitor metric needs to be scoped to an experiment by using some unique string or ID
-*/
-setTimeout(function() {
-
-  try {
-    var forwardToPJS = {
-      mode: extension.mode,
-      return_visitor_evt_apiname: extension.return_visitor_evt_apiname,   
-      experimentId: experimentId,
-      campaignId: campaignId,
-      variationId: variationId,
-      isHoldback: isHoldback
-    };
-    window.__optReturnVisitorTracker.setParams(forwardToPJS);
-    window.__optReturnVisitorTracker.decisionMade(experimentId);
-  } catch(err) { console.log(err); }
-  
-}, 0);
+window.optimizely = window.optimizely || [];
+window.optimizely.push({
+  type: "addListener",
+  filter: {
+    "type": "lifecycle",
+    "name": "activated" // happens before init, act
+  },
+  handler: function(event) {
+    try {
+      var forwardToPJS = {
+        mode: extension.mode,
+        return_visitor_evt_apiname: extension.return_visitor_evt_apiname,   
+        experimentId: experimentId,
+        campaignId: campaignId,
+        variationId: variationId,
+        isHoldback: isHoldback
+      };
+      window.__optReturnVisitorTracker.setParams(forwardToPJS);
+      window.__optReturnVisitorTracker.decisionMade(experimentId);
+    } catch(err) { console.log(err); }    
+  }
+});   
