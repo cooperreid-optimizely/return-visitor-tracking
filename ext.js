3826011ccc7e8b03da28e35078e3a0631bd34423
Cooper Reid <cooper.reid@optimizely.com>
2019-05-16 12:41:52 -0400

    Update ext.js

--- ext.js
+++ ext.js
@@ -1,29 +1,35 @@
 window.optimizely = window.optimizely || [];
-window.optimizely.push({
-  type: "addListener",
-  filter: {
-    "type": "lifecycle",
-    "name": "activated"
-  },
-  handler: function(event) {
-    try {
-      if(!!localStorage.getItem('__optReturnVisitorTrackerDebug')) {
-        console.log('+++ In the Ext handler', (new Date).getTime());
-      }
-      var forwardToPJS = {
-        mode: extension.mode,
-        return_visitor_evt_apiname: extension.return_visitor_evt_apiname,   
-        experimentId: experimentId,
-        campaignId: campaignId,
-        variationId: variationId,
-        isHoldback: isHoldback
-      };
-      window.__optReturnVisitorTracker.setParams(forwardToPJS);
-      window.__optReturnVisitorTracker.decisionMade(experimentId);
-    } catch(err) { console.log(err); }    
-  }
-});   
 
+var runExt = function() {
+ try {
+    if(!!localStorage.getItem('__optReturnVisitorTrackerDebug')) {
+      console.log('+++ In the Ext handler', (new Date).getTime());
+    }
+    var forwardToPJS = {
+      mode: extension.mode,
+      return_visitor_evt_apiname: extension.return_visitor_evt_apiname,   
+      experimentId: experimentId,
+      campaignId: campaignId,
+      variationId: variationId,
+      isHoldback: isHoldback
+    };
+    window.__optReturnVisitorTracker.setParams(forwardToPJS);
+    window.__optReturnVisitorTracker.decisionMade(experimentId);
+  } catch(err) { console.log(err); }     
+}
+
+if(window.__optReturnVisitorTracker.initialized) runExt();
+else {
+  window.optimizely.push({
+    type: "addListener",
+    filter: {
+      "type": "lifecycle",
+      "name": "activated"
+    },
+    handler: runExt
+  });   
+}
+  
 if(!!localStorage.getItem('__optReturnVisitorTrackerDebug')) {
   console.log('+++ In the Ext Body', (new Date).getTime());
 }
